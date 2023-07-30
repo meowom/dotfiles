cc7f93aa562662c924a4f50eb7148248bff5da92
yiqing <yiqing945@gmail.com>
2023-07-30 16:36:57 +0800

    Update .zshrc Update 03_install-python.sh

--- run_onchange_after_03_install-python.sh
+++ run_onchange_after_03_install-python.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
-
 # Check the operating system
+echo "Installing Pyenv..."
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # macOS
   brew install pyenv
@@ -10,7 +10,9 @@ elif [[ -f "/etc/arch-release" ]]; then
 else
   echo "Error: Unsupported operating system."
   exit 1
+fi
 # Set up shell environment for Pyenv
+echo 'test'
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
@@ -20,5 +22,4 @@ echo "Installing Python..."
 pyenv install 3.9.6  # Replace with your desired Python version
 pyenv global 3.9.6    # Set the installed Python version as the global default
 echo "Pyenv and Python installation completed!"
-fi
 
