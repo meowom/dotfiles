1e3aa3f2f8baeb676a94f41480444d22e0a83a1e
yiqing <yiqing945@gmail.com>
2023-08-01 13:20:00 +0800

    Update .config/nvim/chatgpt-actions.json Update .oh-my-zsh/custom/zellij.zsh Update .zshrc

--- dot_oh-my-zsh/custom/zellij.zsh
+++ dot_oh-my-zsh/custom/zellij.zsh
@@ -1,15 +1,17 @@
 
-# Run Zellij with a specific layout
-zellij_with_layout() {
-    zellij --layout "$@"
-}
-compdef '_files -W ~/.config/zellij/layouts -/' zellij_with_layout
-alias zjl='zellij_with_layout'
-
 # Attach to an existing Zellij session
 _zellij_attach() {
   local sessions
   sessions=($(zellij list-sessions | grep -oE '^\S+'))
   _describe 'values' sessions
 }
+alias zja='zellij attach'
 compdef _zellij_attach zja
+
+# Run Zellij with a specific layout
+zellij_with_layout() {
+    zellij --layout "$HOME/.config/zellij/layouts/$1"
+}
+compdef '_files -W ~/.config/zellij/layouts -/' zellij_with_layout
+alias zjl='zellij_with_layout'
+
