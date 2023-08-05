05766c4a1af82b07c845ec0d3c82c11faf918856
MEOW <meow@MEOWs-MBP.lan>
2023-08-05 15:57:19 +0800

    .

--- run_onchange_01_install-packages.sh
+++ run_onchange_01_install-packages.sh
@@ -1,13 +1,25 @@
 #!/bin/zsh
+
+# Common packages for searching, finding, and replacing text
+search_packages="ripgrep fd sd"
+
+# Terminal enhancement packages
+terminal_packages="gitui bottom bat broot zellij"
+
+# Productivity tools
+productivity_packages="neovim chezmoi tealdeer"
+
 # Check the operating system
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # macOS
-  brew install neovim zellij chezmoi gh tealdeer gitui dust ripgrep bottom fd sd lsd zoxide 
-  # Show in Finder -> Right click -> Open
+  brew install $search_packages $terminal_packages $productivity_packages
+  brew install gh
+  # To access, find it in Finder, right-click, and select "Open"
   brew install --cask alacritty
 elif [[ -f "/etc/arch-release" ]]; then
   # Arch Linux
-  sudo pacman -Syu zellij neovim chezmoi git github-cli man-db man-pages base-devel 
+  sudo pacman -Syu $search_packages $terminal_packages $productivity_packages 
+  sudo pacman -Syu github-cli git man-db man-pages base-devel
 else
   echo "Error: Unsupported operating system."
   exit 1
