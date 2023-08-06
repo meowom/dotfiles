0ed39cbf1ff34d6f21c2d7868514021beffd3eac
MEOW <meow@MEOWs-MBP.lan>
2023-08-06 09:09:21 +0800

    Update .oh-my-zsh/custom/alias.zsh Update 01_install-packages.sh

--- dot_oh-my-zsh/custom/alias.zsh
+++ dot_oh-my-zsh/custom/alias.zsh
@@ -3,6 +3,7 @@ alias cmu='chezmoi update'
 alias cma='chezmoi add'
 alias cms='chezmoi status'
 
+
 # TODO 
 # alias cmaa = 'chezmoi add *'
 
--- run_onchange_01_install-packages.sh
+++ run_onchange_01_install-packages.sh
@@ -3,14 +3,14 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # macOS
   brew install ripgrep fd sd
-  brew install lazygit bottom zellij
+  brew install lazygit bottom zellij dust
   brew install neovim chezmoi tealdeer tree
   brew install gh
   brew install --cask alacritty
 elif [[ -f "/etc/arch-release" ]]; then
   # Arch Linux
   sudo pacman -Syu ripgrep fd sd
-  sudo pacman -Syu lazygit bottom zellij
+  sudo pacman -Syu lazygit bottom zellij dust
   sudo pacman -Syu neovim chezmoi tealdeer tree
   sudo pacman -Syu github-cli git man-db man-pages base-devel
 else
