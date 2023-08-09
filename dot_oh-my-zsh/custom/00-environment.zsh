7eb05c0406e42346dc97de22699a5ebd191a7009
MEOW <meow@MEOWs-MBP.lan>
2023-08-09 23:00:34 +0800

    Add .oh-my-zsh/custom/00-environment.zsh Add .oh-my-zsh/custom/01-aliases.zsh Add .oh-my-zsh/custom/chezmoi.zsh Add .oh-my-zsh/custom/plugins/example/example.plugin.zsh Add .oh-my-zsh/custom/themes/example.zsh-theme

---
+++ dot_oh-my-zsh/custom/00-environment.zsh
@@ -0,0 +1,1 @@
+export TOOLS_PATH=""
---
+++ dot_oh-my-zsh/custom/01-aliases.zsh
@@ -0,0 +1,10 @@
+# Group: chezmoi
+alias cmu='chezmoi update'
+alias cma='chezmoi add'
+alias cms='chezmoi status'
+
+
+# TODO 
+# alias cmaa = 'chezmoi add *'
+
+alias vi="nvim"
