622d152a752c6bed60e7dbc4844d326e1308d7a8
MEOW <meow@MEOWs-MBP.lan>
2023-12-15 16:07:37 +0800

    Update .zshrc Update 15_install-python.sh

--- run_onchange_after_15_install-python.sh
+++ run_onchange_after_15_install-python.sh
@@ -1,17 +1,37 @@
 #!/bin/zsh
-# Check the operating system
-echo "Install Python..."
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  # macOS
-  brew install python poetry
-elif [[ -f "/etc/arch-release" ]]; then
-  # Arch Linux
-  sudo pacman -S python-poetry
-else
-  echo "Error: Unsupported operating system."
-  exit 1
+
+# Function to install a package using Homebrew or Pacman
+install_package() {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install $1
+  elif [[ -f "/etc/arch-release" ]]; then
+    # Use 'python-poetry' for Arch Linux
+    local package_name=$1
+    if [ "$package_name" == "poetry" ]; then
+      package_name="python-poetry"
+    fi
+    sudo pacman -S $package_name
+  else
+    echo "Error: Unsupported operating system."
+    exit 1
+  fi
+}
+
+# Check if Pyenv is installed and install it if not
+if ! command -v pyenv >/dev/null 2>&1; then
+  echo "Installing Pyenv..."
+  install_package pyenv
 fi
 
+# Install Python and set it as the global default
+echo "Installing Python..."
+pyenv install 3.11.2  # Replace with your desired Python version
+pyenv global 3.11.2
 source ~/.zshrc
 
-echo "Python installation completed!"
+# Install Poetry
+echo "Installing Poetry..."
+install_package poetry
+
+echo "Pyenv, Python, and Poetry installation completed!"
+
