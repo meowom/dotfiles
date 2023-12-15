01b030b30a98324c4e2c7079cdcd1f53e4c65e75
MEOW <meow@MEOWs-MBP.lan>
2023-12-15 15:17:48 +0800

    Update .config/nvim/lua/config/options.lua

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+  vim.cmd("cd ~/projects/notes")
+  vim.o.guifont = "Hack Nerd Font Mono:h18" -- text below applies for VimScript
+  vim.g.neovide_cursor_animation_length = 0
+end
