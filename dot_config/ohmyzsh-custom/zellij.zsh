357e8ca71cbc8005422ba4b1d3eed80822a533cb
MEOW <meow@MEOWs-MBP.lan>
2023-12-19 15:39:01 +0800

    Update .config/ohmyzsh-custom/zellij.zsh

--- dot_config/ohmyzsh-custom/zellij.zsh
+++ dot_config/ohmyzsh-custom/zellij.zsh
@@ -1,15 +1,3 @@
-# Attach to an existing Zellij session
-_zellij_attach() {
-  local sessions
-  sessions=($(zellij list-sessions | grep -oE '^\S+'))
-  _describe 'values' sessions
-}
-zellij_with_attach() {
-  zellij attach $1
-}
-compdef _zellij_attach zellij_with_attach
-alias zja='zellij_with_attach'
-
 # Run Zellij with a specific layout
 zellij_with_layout() {
     zellij --layout "$HOME/.config/zellij/layouts/$1"
@@ -17,3 +5,4 @@ zellij_with_layout() {
 compdef '_files -W ~/.config/zellij/layouts -/' zellij_with_layout
 alias zjl='zellij_with_layout'
 
+alias zja='zellij ls | fzf -0 -1 | xargs -to zellij a'
